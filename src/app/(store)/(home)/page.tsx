043a9f8039d2f65e3dc84d0df21af5201a983718
Pedro Caetano <pedroccaetano@live.com>
2024-08-20 21:53:54 +0100

    feat: implement home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,5 +1,73 @@
-export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
+import Link from 'next/link'
+import Image from 'next/image'
 
-  return <main>Home</main>
+export default function Home() {
+  return (
+    <div className="grid max-h-[800px] grid-cols-9 grid-row-6 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src="/moletom-never-stop-learning.png"
+          width={920}
+          height={920}
+          quality={100}
+          alt=""
+          className="group-hover:scale-105 transition-transform duration-500"
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-6">
+          <span className="text-sm truncate">Moleton Never Stop Learning</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src="/moletom-java.png"
+          width={920}
+          height={920}
+          quality={100}
+          alt=""
+          className="group-hover:scale-105 transition-transform duration-500"
+        />
+
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-6">
+          <span className="text-sm truncate">
+            Moletom Coffee Is All You Need
+          </span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src="/camiseta-dowhile-2022.png"
+          width={920}
+          height={920}
+          quality={100}
+          alt=""
+          className="group-hover:scale-105 transition-transform duration-500"
+        />
+
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-6">
+          <span className="text-sm truncate">Moletom Expand Your Mind</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+    </div>
+  )
 }
