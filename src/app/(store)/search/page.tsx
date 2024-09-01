5bec3e93ac751034bf48b4affba8362a5dac6934
Pedro Caetano <pedroccaetano@live.com>
2024-09-01 16:09:47 +0100

    feat: mock layout search page

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,5 +1,77 @@
-export default async function Search() {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
+import Link from 'next/link'
+import Image from 'next/image'
 
-  return <main>Search</main>
+const product = {
+  id: 1,
+  title: 'Moletom Never Stop Learning',
+  slug: 'moletom-never-stop-learning',
+  price: 129,
+  image: '/moletom-never-stop-learning.png',
+  description: 'Moletom fabricado com 88% de algodão e 12% de poliéster.',
+  featured: true,
+}
+
+export default async function Search() {
+  return (
+    <div className="flex flex-col gap-4">
+      <p>
+        Resultados para: <span className="font-semibold">moletom</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Link
+          key={product.id}
+          href={`/product/${product.slug}`}
+          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+        >
+          <Image
+            src={product.image}
+            width={480}
+            height={480}
+            quality={100}
+            alt=""
+            className="group-hover:scale-105 transition-transform duration-500"
+          />
+
+          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-6">
+            <span className="text-sm truncate">{product.title}</span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {product.price.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </Link>
+        <Link
+          key={product.id}
+          href={`/product/${product.slug}`}
+          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+        >
+          <Image
+            src={product.image}
+            width={480}
+            height={480}
+            quality={100}
+            alt=""
+            className="group-hover:scale-105 transition-transform duration-500"
+          />
+
+          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-6">
+            <span className="text-sm truncate">{product.title}</span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {product.price.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
 }
