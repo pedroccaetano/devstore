e8e18e6ef385bf5b056b25e04e25733755d6b906
Pedro Caetano <pedroccaetano@live.com>
2024-09-01 17:45:30 +0100

    feat: test add product to cart

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,40 @@
+describe('add product to car', () => {
+  it('should be able to navigate to the product page and add it to the cart', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('contain', '/product')
+
+    cy.contains('Cart (0)').should('exist')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should not count duplicated products on cart', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('contain', '/product')
+
+    cy.contains('Cart (0)').should('exist')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('contain', '/product')
+
+    cy.contains('Cart (0)').should('exist')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+  })
+})
